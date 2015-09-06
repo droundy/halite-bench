c9475ce1d150335b34303e067fcfc8b09558c444
David Roundy <roundyd@physics.oregonstate.edu>
2015-09-06 14:52:59 -0400

    enable comparison with sodiumoxide

---
+++ README.md
@@ -0,0 +1,6 @@
+# halite-bench
+
+A benchmark program for comparing various NaCl impementations that can
+be used in rust.  To run the benchmark just do:
+
+    cargo run --release
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 extern crate time;
 extern crate onionsalt;
+extern crate sodiumoxide;
 
 use time::PreciseTime;
 use onionsalt::crypto;
 
 fn main() {
     bench_random_nonce();
+    bench_sodium_gen_nonce();
     for i in &[33usize, 64, 100, 1000, 10000] {
+        println!("");
         bench_secretbox(*i);
+        bench_sodium_secretbox(*i);
         bench_cryptobox(*i);
+        bench_sodium_cryptobox(*i);
     }
 }
 
@@ -46,6 +51,12 @@ fn bench_random_nonce() {
     });
 }
 
+fn bench_sodium_gen_nonce() {
+    bench_function("sodium gen_nonce", 1, |_, _| {
+        sodiumoxide::crypto::secretbox::gen_nonce();
+    });
+}
+
 fn bench_cryptobox(len: usize) {
     let k1 = crypto::box_keypair().unwrap();
     let k2 = crypto::box_keypair().unwrap();
@@ -62,3 +73,28 @@ fn bench_secretbox(len: usize) {
         crypto::secretbox(c, p, &n, &k.0).unwrap();
     });
 }
+
+fn bench_sodium_secretbox(len: usize) {
+    let k = sodiumoxide::crypto::secretbox::gen_key();
+    let n = sodiumoxide::crypto::secretbox::gen_nonce();
+    bench_function(&format!("sodium secretbox({})", len), len, |c,p| {
+        let ciphertext = sodiumoxide::crypto::secretbox::seal(&p[32..], &n, &k);
+        for i in 0..len-32 {
+            c[i+32] = ciphertext[i];
+        }
+    });
+}
+
+fn bench_sodium_cryptobox(len: usize) {
+    let (_, oursk) = sodiumoxide::crypto::box_::gen_keypair();
+    // normally theirpk is sent by the other party
+    let (theirpk, _) = sodiumoxide::crypto::box_::gen_keypair();
+    let nonce = sodiumoxide::crypto::box_::gen_nonce();
+    bench_function(&format!("cryptobox({})", len), len, |c,p| {
+        let ciphertext = sodiumoxide::crypto::box_::seal(&p[32..], &nonce, &theirpk, &oursk);
+        for i in 0..len-32 {
+            c[i+32] = ciphertext[i];
+        }
+    });
+}
+
